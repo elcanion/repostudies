6d653890366b4b4d1f17115e1aa28e2bc21ed1c1
elcanion <elcaniondev@gmail.com>
2022-07-25 00:01:58 -0300

    Bug fix on line 6

--- Program.cs
+++ Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Bugfix 3");
 Console.WriteLine("Trying again");
 Console.WriteLine("Let's say we just made a bugfix");
 Console.WriteLine("Bugfix 2");
+Console.WriteLine("Bugfix 5");
