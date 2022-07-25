e51316cfb0247452666a5c5391a7b7fd8fcba665
elcanion <elcaniondev@gmail.com>
2022-07-24 23:57:54 -0300

    Bug fix on line 5

--- Program.cs
+++ Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("This time, the pull request will be approved");
 Console.WriteLine("Trying again");
 Console.WriteLine("Let's say we just made a bugfix");
+Console.WriteLine("Bugfix 2");
