e7169dea706bd642ef869ee85cd80b27f1ad2031
elcanion <elcaniondev@gmail.com>
2022-07-24 23:54:41 -0300

    Bug fix on line 4

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("This is refactoring 7");
 Console.WriteLine("This time, the pull request will be approved");
 Console.WriteLine("Trying again");
+Console.WriteLine("Let's say we just made a bugfix");
