1791ce425b42a70ae70bbcff5b1ec083084837cd
elcanion <elcaniondev@gmail.com>
2022-07-24 23:41:25 -0300

    Mudança de texto no Program.cs [refactoring 6]

--- Program.cs
+++ Program.cs
@@ -1,6 +1,1 @@
-﻿Console.WriteLine("This is the modified version - Refactoring 3");
-Console.WriteLine("Random text 1");
-Console.WriteLine("Random text 2");
-Console.WriteLine("Random text 3");
-Console.WriteLine("This is Refactoring 4");
-Console.WriteLine("This is Refactoring 5");
+﻿Console.WriteLine("This is refactoring 6");
