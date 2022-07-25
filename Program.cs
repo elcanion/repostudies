d3970be9097041f76dab5ec1407daa72de8b497b
elcanion <elcaniondev@gmail.com>
2022-07-24 23:58:39 -0300

    Bug fix on line 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("This is refactoring 7");
+﻿Console.WriteLine("Bugfix 4");
 Console.WriteLine("Bugfix 3");
 Console.WriteLine("Trying again");
 Console.WriteLine("Let's say we just made a bugfix");
